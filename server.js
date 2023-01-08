f098fc581023d0a6b5976e23721b18336d01f3cb
DanaAmine <danaamine080807@gmail.com>
2023-01-08 13:43:47 +0100

    db connection done

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const ConnectDB = require("./DB/db_init.js");
 const app = express();
 const PORT = 8080;
+ConnectDB();
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});  
+
+
+
+
