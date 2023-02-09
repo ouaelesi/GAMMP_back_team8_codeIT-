c2c08b8daa926bda2b522deb81d454e0eedf3baa
Lenasaidi <saidilena6@gmail.com>
2023-02-09 18:36:59 +0100

    updates in the activities controller (post activity)

--- app/controllers/activitiesController.js
+++ app/controllers/activitiesController.js
@@ -1,4 +1,6 @@
 const Activity = require('../models/Activity') ;
+const Rank = require('../models/Rank');
+const User = require("../models/User");
 
 module.exports.get = async (req, res) =>{
     const _id = req.params.id;
@@ -22,6 +24,15 @@ module.exports.post = async (req, res) =>{
     try{
         const activity = await Activity.create(req.body);
         res.status(200).send(activity);
+        const leaders = await activity.teamLeaders.find();
+        leaders.forEach( leader => {
+            leader.rank.totalAddedPoints += 10;
+            // const ranks = await Rank.find({}, {history: 0 })
+        });
+        const organizers = await activity.organizers.find();
+        organizers.forEach( organizer => {
+            organizer.rank.totalAddedPoints += 5;
+        });
     }catch(err){
         console.log("creation failed ");
         res.status(500).json({message: err.message});
