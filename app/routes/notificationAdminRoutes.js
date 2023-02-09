fc0771e1089bec392468d437495ede597065ba65
Ayyoub Kasmi <ayyoubkasmidz@gmail.com>
2023-02-09 22:21:45 +0100

    fixed inactivity auto-verification time

--- app/routes/notificationAdminRoutes.js
+++ app/routes/notificationAdminRoutes.js
@@ -5,8 +5,7 @@ const router = Router();
 
 // 86400 seconds = 24h
 
-// setInterval(notificationAdminController.verifyInactiveMembers, 86400*1000);
-setInterval(notificationAdminController.verifyInactiveMembers, 3000);
+setInterval(notificationAdminController.verifyInactiveMembers, 86400*1000);
 
 //to test the api use SetTimeout
 // setTimeout(notificationAdminController.verifyInactiveMembers, 3000);
